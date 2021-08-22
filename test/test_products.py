34fa0b144cb1c2d11fdf4e33d00f3d73cc445ff7
Shawn <shawntaylor@weecology.org>
2021-08-22 11:22:58 -0600

    some more tests

---
+++ test/test_products.py
@@ -0,0 +1,68 @@
+import pytest
+import numpy as np
+
+from pyUnpackQA import (unpack_to_array,
+                        unpack_to_dict, 
+                        list_products,
+                        list_qa_flags
+                        )
+
+"""
+Just iterating thru all products as integration tests.
+"""
+
+qa_array = np.array([[8,8,8],
+                     [16,16,16],
+                     [256,256,256]])
+
+all_product_identifiers = list_products(sensor='all')
+
+@pytest.mark.parametrize('product', all_product_identifiers)
+def test_qa_flag_list(product):
+    """Lists of flags should be available for each product"""
+    flags = list_qa_flags(product = product)
+    assert len(flags) > 0
+
+@pytest.mark.parametrize('product', all_product_identifiers)
+def test_unpack_array_shape(product):
+    """ 
+    unpack_to_array result should match the input shape and have
+    a new axis at position (-1) with the same length as number of flags.
+    """
+    n_flags = len(list_qa_flags(product = product))
+    target_shape = qa_array.shape + (n_flags,)
+    
+    result = unpack_to_array(qa_array, product = product)
+    assert result.shape == target_shape
+
+@pytest.mark.parametrize('product', all_product_identifiers)
+def test_unpack_dict_shape(product):
+    """Each dictionary value in unpack_to_dick should match the input qa shape"""
+    flags = list_qa_flags(product = product)
+    target_shape = qa_array.shape
+    
+    result = unpack_to_dict(qa_array, product = product, flags='all')
+    
+    all_shapes_good = [result[flag].shape == target_shape for flag in flags]
+    assert all(all_shapes_good)
+
+@pytest.mark.parametrize('product', all_product_identifiers)
+def test_unpack_dict_all_flags(product):
+    """ The all flag indicator returns all available flags"""
+    flags = list_qa_flags(product = product)
+    
+    result = unpack_to_dict(qa_array, product = product, flags='all')
+    all_flags_in_result = [f in result for f in flags]
+    all_results_in_flags = [f in flags for f in result.keys()]
+    assert all(all_flags_in_result) and all(all_results_in_flags)
+    
+@pytest.mark.parametrize('product', all_product_identifiers)
+def test_unpack_dict_2_flags(product):
+    """ Get the first and last flag only. """
+    flags = list_qa_flags(product = product)
+    flags = [flags[0], flags[-1]]
+    
+    result = unpack_to_dict(qa_array, product = product, flags=flags)
+    all_flags_in_result = [f in result for f in flags]
+    all_results_in_flags = [f in flags for f in result.keys()]
+    assert all(all_flags_in_result) and all(all_results_in_flags)
