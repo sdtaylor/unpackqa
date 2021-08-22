96df6d9ee18c4c30309a23be29bb8ffb513aa578
Shawn <shawntaylor@weecology.org>
2021-08-22 10:23:51 -0600

    update tests

--- test/test_validation.py
+++ test/test_validation.py
@@ -1,35 +1,45 @@
 import pytest
 import numpy as np
 
-from pyUnpackQA.Landsat import L8C2L2_QAPixel
+from pyUnpackQA import unpack_to_array, unpack_to_dict
 
-l8 = L8C2L2_QAPixel()
+default_product = 'L8C2L2_QAPixel'
 
 def test_max_value():
     """Raise error when values is too large"""
     too_large_qa = np.array([1e6, 2e6], dtype=np.int64)
     with pytest.raises(ValueError):
-        l8.unpack_to_dict(too_large_qa)
+        unpack_to_dict(too_large_qa, product = default_product)
 
 def test_neg_values():
     """bit arrays should not have negative numbers"""
     neg_qa = np.array([-2,-3], dtype=np.int64)
     with pytest.raises(ValueError):
-        l8.unpack_to_dict(neg_qa)
+        unpack_to_dict(neg_qa, product = default_product)
     
 def test_array_type():
     """only integer arrays should be passed"""
     qa = np.array([1024,2048], dtype=np.float64)
     with pytest.raises(TypeError):
-        l8.unpack_to_dict(qa)
+        unpack_to_dict(qa, product = default_product)
 
 def test_array():
     """Only numpy arrays"""
     qa = [128,128]
     with pytest.raises(TypeError):
-        l8.unpack_to_dict(qa)
+        unpack_to_dict(qa)
+
+def test_single_value_int():
+    """A singel value is fine as long as it's an int"""
+    result = unpack_to_array(1, product = default_product)
+    assert result is not None
+    
+def test_single_value_float():
+    """No floats please"""
+    with pytest.raises(TypeError):
+        unpack_to_array(1.1, product = default_product)
 
 def test_invalid_flag():
     qa = np.array([64,128], dtype=np.int16)
     with pytest.raises(ValueError):
-        l8.unpack_to_dict(qa, flags=['asdf'])
+        unpack_to_dict(qa, product = default_product, flags=['asdf'])
