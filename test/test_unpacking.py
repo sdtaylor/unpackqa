2f07f6deb651e35461501cd7fc8ec846139d8259
Shawn <shawntaylor@weecology.org>
2021-08-23 08:59:53 -0600

    more substantive shape retention tests

--- test/test_unpacking.py
+++ test/test_unpacking.py
@@ -5,6 +5,10 @@ from pyUnpackQA.tools.unpackbits import unpackbits, int16_to_bits
 
 
 def test_compare_unpack_methods():
+    """
+    Core unpacking function should have the exact results as a slower 
+    string based method
+    """
     arr = np.arange(2046)
     
     method1 = np.array([int16_to_bits(int(x)) for x in arr], dtype=np.uint8)
@@ -16,6 +20,30 @@ def test_compare_unpack_methods():
     assert (method1 == method2).all()
 
 def test_unpackbits_shape_retention():
-    arr = np.random.randint(low=0, high=2048, size=128*128).reshape((128,128))
-    bits = unpackbits(arr, num_bits = 16)
-    assert bits.shape == (16,128,128)
+    """
+    Core unpacking function should take an arbitrary shape and return the
+    same with 1 new axis of length num_bits.
+    """
+    arr1 = np.random.randint(low=0, high=2048, size=64)
+    arr2 = np.random.randint(low=0, high=2048, size=64**2).reshape((64,64))
+    arr3 = np.random.randint(low=0, high=2048, size=64**3).reshape((64,64,64))
+    arr4 = np.random.randint(low=0, high=2048, size=64*21*42).reshape((64,21,42))
+    
+    test_result = [
+        unpackbits(arr1, num_bits=8).shape == (8,64),
+        unpackbits(arr2, num_bits=8).shape == (8,64,64),
+        unpackbits(arr3, num_bits=8).shape == (8,64,64,64),
+        unpackbits(arr4, num_bits=8).shape == (8,64,21,42),
+        
+        unpackbits(arr1, num_bits=16).shape == (16,64),
+        unpackbits(arr2, num_bits=16).shape == (16,64,64),
+        unpackbits(arr3, num_bits=16).shape == (16,64,64,64),
+        unpackbits(arr4, num_bits=16).shape == (16,64,21,42),
+        
+        unpackbits(arr1, num_bits=32).shape == (32,64),
+        unpackbits(arr2, num_bits=32).shape == (32,64,64),
+        unpackbits(arr3, num_bits=32).shape == (32,64,64,64),
+        unpackbits(arr4, num_bits=32).shape == (32,64,21,42)
+        ]
+    
+    assert all(test_result)
