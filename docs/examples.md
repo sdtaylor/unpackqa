9c441fccf63b91356e9993d95b231b5171a46c89
Shawn <shawntaylor@weecology.org>
2021-09-03 16:22:02 -0600

    add modis time series example

--- docs/examples.md
+++ docs/examples.md
@@ -1,3 +1,4 @@
 # Examples
 
 - <a href="examples/Landsat8_scene_masks.html" target="_blank">Making QA masks from a Landsat 8 scene</a>
+- <a href="examples/MODIS_Time_Series.html" target="_blank">QA masks for MODIS NDVI time series from Google Earth Engine</a>
---
+++ docs/examples_dev/MODIS_timeseries.py
@@ -0,0 +1,57 @@
+import ee
+from gee_subset.gee_subset import gee_subset
+import pandas as pd
+import seaborn as sns
+import numpy as np
+# ee.Authenticate()
+ee.Initialize()
+
+import pyUnpackQA
+
+
+# The MODIS vegetation product has a Summary QA band which has 4 values:
+#    0: Good data, use with confidence
+#    1: Marginal data, useful but look at detailed QA for more information
+#    2: Pixel covered with snow/ice
+#    3: Pixel is cloudy
+# It also a a DetailedQA band which is bit packed and gives more information about
+# the pixel quality. Here we'll unpack the DetailedQA band to see. 
+
+# This uses the gee_subset packge to query Google Earth Engine (GEE) for a single
+# pixel timeseries across ~20 years. To use it you'll need a GEE account.
+# - [GEE Info](https://developers.google.com/earth-engine/python_install)
+# - [gee_subset package](https://github.com/bluegreen-labs/gee_subset)
+
+
+df = gee_subset(product    = 'MODIS/006/MOD13A1',
+                bands      = ['NDVI','DetailedQA'],
+                scale      = 500,
+                start_date = '2015-01-01',
+                end_date   = '2020-12-31',
+                latitude   =   44.8166,
+                longitude  = -114.8177,
+                pad        =  0)
+
+# Adjust NDVI by the scaling factor and drop any NA values
+df['NDVI'] = df.NDVI * 0.0001
+df = df[~df.NDVI.isna()].reset_index()
+
+# pyUnpackQA only works on integers. Here DetailedQA are 16-bits
+df['DetailedQA'] = df.DetailedQA.astype(np.uint16)
+
+expanded_detailed_qa = pyUnpackQA.unpack_to_dict(df.DetailedQA.values, product = 'MOD13Q1v006_DetailedQA', flags='all')
+
+# This produces a dictionary where each key is a flag, and each value is an 
+# array the same length as the df data.frame
+# This can be converted directly to a new data.frame where the columsn are the flags,
+# and then appended to the original data.frame.
+expanded_detailed_qa = pd.DataFrame(expanded_detailed_qa)
+df = pd.concat([df, expanded_detailed_qa], axis=1)
+
+# each pixel value in the time series now has the associated flags as columns.
+# The VI_Quality flag has 3 values, where 0 signifies the best quality, 1 signifies a QA issue, and 2 indicates likely clouds. 
+colors = {0:'green',
+          1:'red',
+          2:'darkred'}
+
+sns.scatterplot(x='date',y='NDVI',hue='VI_Quality',data=df,palette=colors)
